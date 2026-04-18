08ca2ece66fc2caa5834b099f3bc75df0b5fe585
FredArgoX <fredy.argo.3@gmail.com>
2026-04-17 23:45:37 -0600

    froker01

---
+++ js/particles.js
@@ -0,0 +1,74 @@
+particlesJS("particles-js", {
+"particles": {
+    "number": {
+    "value": 100,
+    "density": {
+        "enable": true,
+        "value_area": 800
+    }
+    },
+    "color": {
+    "value": "#1D4ED8"
+    },
+    "shape": {
+    "type": "circle",
+    "stroke": {
+        "width": 0,
+        "color": "#7200a7"
+    }
+    },
+    "opacity": {
+    "value": 0.8,
+    "random": false,
+    "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+    }
+    },
+    "size": {
+    "value": 0,
+    "random": true
+    },
+    "line_linked": {
+    "enable": true,
+    "distance": 150,
+    "color": "#920000",
+    "opacity": 1.0,
+    "width": 2.0
+    },
+    "move": {
+    "enable": true,
+    "speed": 3,
+    "direction": "none",
+    "random": true,
+    "straight": false,
+    "out_mode": "out"
+    }
+},
+"interactivity": {
+    "detect_on": "canvas",
+    "events": {
+    "onhover": {
+        "enable": true,
+        "mode": "repulse"
+    },
+    "onclick": {
+        "enable": true,
+        "mode": "push"
+    },
+    "resize": true
+    },
+    "modes": {
+    "repulse": {
+        "distance": 100,
+        "duration": 0.4
+    },
+    "push": {
+        "particles_nb": 4
+    }
+    }
+},
+"retina_detect": true
+});
